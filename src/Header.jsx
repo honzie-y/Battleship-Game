6043eefa8e8171b769362b08abf8e068f19f027c
honzie12 <yu.hongz@northeastern.edu>
2025-03-10 22:04:01 -0700

    implement header title fonts

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <>
     <header className="pt-[20px] flex flex-row justify-between mb-6">
-        <img className="filter-(--filter-logo) rounded-xl" src="/src/assets/battleshiponline-logo.png" alt="battleship-logo"/>
+        <div className="flex gap-2">
+            <img className="filter-(--filter-logo) rounded-xl" src="/src/assets/battleshiponline-logo.png" alt="battleship-logo"/>
+            <h1 className="text-5xl font-rubik-pixels mt-auto">BattleShip Game</h1>
+        </div>
         <NavBar />
     </header>
 
