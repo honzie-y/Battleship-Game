e707357129bb6bc7adac7615497b923e83d9f91e
honzie12 <yu.hongz@northeastern.edu>
2025-03-14 15:32:29 -0700

    made static game page

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 const MobileNav = ({navRef}) => {
   return (
-    <div className="fixed right-1 w-25 pb-2 text-center bg-yellow-800/95 rounded-2xl" ref={navRef}>
+    <div className="fixed right-1 px-2 py-2 text-center bg-yellow-800/95 rounded-2xl" ref={navRef}>
         <nav>
             <ul className="grid grid-rows-3 gap-3">
                 <li>
-                    <Link to={"/"}>Home</Link>
+                    <Link to={"/"} className="hover:bg-yellow-900 rounded-xl px-2">Home</Link>
                 </li>
                 <li>
-                    <Link to="/rules">Game Rules</Link>
+                    <Link to="/rules" className="hover:bg-yellow-900 rounded-xl px-2">Game Rules</Link>
                 </li>
                 <li>
-                    <Link to={"/highscores"}>High Scores</Link>
+                    <Link to={"/highscores"} className="hover:bg-yellow-900 rounded-xl px-2">High Scores</Link>
                 </li>
             </ul>
         </nav>
