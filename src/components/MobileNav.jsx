ffbfaaa11f8a8254d214c39828d9e3d12daa0a6a
honzie12 <yu.hongz@northeastern.edu>
2025-03-20 17:42:13 -0700

    refactor mobile nav

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,20 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
+const links = [
+    { 
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "Game Rules",
+        path: "/rules"
+    },
+    {
+        name: "High Scores",
+        path: "/highscores"
+    }
+]
 
 const MobileNav = ({navRef}) => {
+
+    const location = useLocation();
+    const currentPath = location.pathname;
+
   return (
     <div className="fixed right-1 px-2 py-2 text-center bg-yellow-800/95 rounded-2xl" ref={navRef}>
         <nav>
             <ul className="grid grid-rows-3 gap-3">
-                <li>
-                    <Link to={"/"} className="hover:bg-yellow-900 rounded-xl px-2">Home</Link>
-                </li>
-                <li>
-                    <Link to="/rules" className="hover:bg-yellow-900 rounded-xl px-2">Game Rules</Link>
-                </li>
-                <li>
-                    <Link to={"/highscores"} className="hover:bg-yellow-900 rounded-xl px-2">High Scores</Link>
-                </li>
+                {
+                    links.map((link, index) => {
+                        return <li><Link to={link.path} key={index} className={`${link.path === currentPath && 'bg-yellow-900'} hover:bg-yellow-900 rounded-xl px-2`}>{link.name}</Link></li>
+                    })
+                }
             </ul>
         </nav>
     </div>
